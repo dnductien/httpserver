0de1d28e5bf4a1662a007379a95f63b5054b35ac
ductien.dangnguyen <ductien.dangnguyen@gmail.com>
2021-09-13 16:37:05 +0200

    Make getstatus code green

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
+    private int statusCode = 200;
+
     public HttpClient(final String host, int port, final String request) throws IOException {
         Socket socket = new Socket(host, port);
         socket.getOutputStream().write(("GET " + request + " HTTP/1.1\r\n" +
@@ -12,15 +14,16 @@ public class HttpClient {
 
         int c;
         while ((c = socket.getInputStream().read()) != -1) {
+            if (c != '\n') break;
             System.out.print((char) c);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        new HttpClient("httpbin.org", 80, "/html");
     }
 
     public int getStatusCode() {
-        return 0;
+        return statusCode;
     }
 }
